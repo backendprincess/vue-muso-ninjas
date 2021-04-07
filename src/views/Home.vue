1ef210aead7e43df390de8dbbf84cb014b050482
backendprincess <lemur.king.julien@gmail.com>
2021-04-07 19:59:11 +0300

    Add ListView component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
       <div v-if="error">Could not fetch the data</div>
-      <div v-if="documents">
-          <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+      <div v-if="documents">          
+          <ListView :playlists="documents"/>
       </div>
   </div>
 </template>
 
 <script>
 import getCollection from '@/composables/getCollection'
+import ListView from '@/components/ListView'
 export default {
     name: 'Home',
+    components: { ListView }, 
     setup() {
       const { documents, error } = getCollection('playlists')
 
