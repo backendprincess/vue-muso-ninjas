ea354a23fbaeda00558b8da005071d5204c2371b
backendprincess <lemur.king.julien@gmail.com>
2021-04-07 18:38:00 +0300

    Add New Playlist component

---
+++ src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,50 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <h4>Create New playlist</h4>
+        <input type="text" required placeholder="Playlist title" v-model="title">
+        <textarea required placeholder="Playlist description..." v-model="description"></textarea>
+        <!-- upload playlist image -->
+        <label>Upload playlist cover image</label>
+        <input type="file">
+
+        <div class="error"></div>
+        <button>Create</button>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup() {
+        const title = ref('')
+        const description = ref('')
+
+        const handleSubmit = () => {
+            console.log(title, description)
+        }
+
+        return { title, description, handleSubmit }
+    }
+}
+</script>
+
+<style>
+    form {
+        background: white;
+    }
+
+    input[type="file"] {
+        border: 0;
+        padding: 0;
+    }
+
+    label {
+        font-size: 12px;
+        display: block;
+        margin-top: 30px;
+    }
+
+    button {
+        margin-top: 20px;
+    }
+</style>
